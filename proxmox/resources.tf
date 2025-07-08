cc124541a5e67e01d97da5bccfca52cf3f6f7c22
Oliver Kelly <foolishhim@gmail.com>
2025-07-08 09:02:55 +0100

    added vault nodes

--- proxmox/resources.tf
+++ proxmox/resources.tf
@@ -27,6 +27,20 @@ locals {
       node    = "pve3"
     }
   }
+  vault_hosts = {
+    "vault-0" = {
+      macaddr = "BC:24:11:1A:3A:3A"
+      node    = "pve1"
+    }
+    "vault-1" = {
+      macaddr = "BC:24:11:E8:9A:A8"
+      node    = "pve2"
+    }
+    "vault-2" = {
+      macaddr = "BC:24:11:65:3A:6E"
+      node    = "pve3"
+    }
+  }
 
 }
 
@@ -37,10 +51,66 @@ resource "proxmox_vm_qemu" "consul_hosts" {
   clone       = var.vm_template
   full_clone  = true
   cpu {
-    cores = 4
+    cores = 2
     type  = "kvm64"
   }
-  memory    = 4096
+  memory    = 2048
+  scsihw    = "virtio-scsi-pci"
+  bootdisk  = "scsi0"
+  os_type   = "cloud-init"
+  agent     = 1
+  tags      = "docker,prod,linux"
+  hagroup   = "prod"
+  hastate   = "started"
+  ciuser    = "oli"
+  sshkeys   = file("~/.ssh/ct-admin.pub")
+  ipconfig0 = "ip=dhcp"
+
+  # VGA
+  vga {
+    type = "std"
+  }
+
+  # System disk
+  disks {
+    ide {
+      ide0 {
+        cloudinit {
+          storage = "ceph_vmdisks"
+        }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          size       = "30G"
+          storage    = "ceph_vmdisks"
+          emulatessd = true
+        }
+      }
+    }
+  }
+
+  network {
+    id      = 0
+    model   = "virtio"
+    bridge  = "PROD"
+    macaddr = each.value.macaddr
+  }
+}
+
+
+resource "proxmox_vm_qemu" "vault_hosts" {
+  for_each = local.vault_hosts
+  name        = each.key
+  target_node = each.value.node
+  clone       = var.vm_template
+  full_clone  = true
+  cpu {
+    cores = 2
+    type  = "kvm64"
+  }
+  memory    = 2048
   scsihw    = "virtio-scsi-pci"
   bootdisk  = "scsi0"
   os_type   = "cloud-init"
@@ -176,6 +246,24 @@ resource "routeros_ip_dhcp_server_lease" "dockerhost_2" {
   server      = "PROD"
 }
 
+resource "routeros_ip_dhcp_server_lease" "vault_0" {
+  address     = "10.18.20.110"
+  mac_address = "BC:24:11:1A:3A:3A"
+  server      = "PROD"
+}
+
+resource "routeros_ip_dhcp_server_lease" "vault_1" {
+  address     = "10.18.20.111"
+  mac_address = "BC:24:11:E8:9A:A8"
+  server      = "PROD"
+}
+
+resource "routeros_ip_dhcp_server_lease" "vault_2" {
+  address     = "10.18.20.112"
+  mac_address = "BC:24:11:65:3A:6E"
+  server      = "PROD"
+}
+
 resource "routeros_ip_dns_record" "consul_0" {
   name    = "consul-0.0lzi.com"
   address = routeros_ip_dhcp_server_lease.consul_0.address
@@ -209,5 +297,23 @@ resource "routeros_ip_dns_record" "dockerhost_1" {
 resource "routeros_ip_dns_record" "dockerhost_2" {
   name    = "dockerhost-2.0lzi.com"
   address = routeros_ip_dhcp_server_lease.dockerhost_2.address
+  type    = "A"
+}
+
+resource "routeros_ip_dns_record" "vault_0" {
+  name    = "vault-0.0lzi.com"
+  address = routeros_ip_dhcp_server_lease.vault_0.address
+  type    = "A"
+}
+
+resource "routeros_ip_dns_record" "vault_1" {
+  name    = "vault-1.0lzi.com"
+  address = routeros_ip_dhcp_server_lease.vault_1.address
+  type    = "A"
+}
+
+resource "routeros_ip_dns_record" "vault_2" {
+  name    = "vault-2.0lzi.com"
+  address = routeros_ip_dhcp_server_lease.vault_2.address
   type    = "A"
 }
