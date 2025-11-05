fe817f1c6086936209a748ceef6e12df3b8a9a55
Oliver Kelly <foolishhim@gmail.com>
2025-11-05 16:12:45 +0000

    update openbao config

--- openbao/policies.tf
+++ openbao/policies.tf
@@ -1,8 +1,16 @@
 data "vault_policy_document" "all" {
   rule {
     path         = "*"
-    capabilities = ["create", "read", "update", "delete", "list"]
+    capabilities = ["sudo", "create", "read", "update", "delete", "list"]
     description  = "allow all on secrets"
+  }
+}
+
+data "vault_policy_document" "readonly" {
+  rule {
+    path         = "*"
+    capabilities = ["read"]
+    description  = "allow readonly on secrets"
   }
 }
 
@@ -10,4 +18,25 @@ resource "vault_policy" "all" {
   name   = "all"
   policy = data.vault_policy_document.all.hcl
 }
+
+resource "vault_policy" "readonly" {
+   name = "read-only"
+   policy = data.vault_policy_document.readonly.hcl
+}
+
+
+resource "vault_jwt_auth_backend_role" "gitlab-jwt" {
+  backend        = vault_jwt_auth_backend.jwt_gitlab.path
+  role_name      = "read-only"
+  token_policies = ["default", "read-only"]
+  bound_audiences = ["https://vault.0lzi.com"]
+  user_claim      = "user_email"
+  token_ttl = "3600"
+  role_type       = "jwt"
+  bound_claims = {
+    project_id = "2,6"
+    ref_type   = "branch"
+  }
+}
+
 
