c90bf032cc6ea2b3934798c406e514d4c56f9963
Oliver Kelly <foolishhim@gmail.com>
2026-05-29 11:43:15 +0100

    add pki engine and approle login
    update approle and added bootstrap auth role
    
    tofu fmt
    
    allow jwt to read approle auth
    
    remove login resource
    
    update so dont need local files

--- openbao/auth.tf
+++ openbao/auth.tf
@@ -1,5 +1,6 @@
 resource "vault_auth_backend" "userpass" {
-  type = "userpass"
+  type        = "userpass"
+  description = "Login with Username and Password"
 }
 
 resource "vault_generic_endpoint" "demo" {
@@ -17,8 +18,41 @@ EOT
 
 # JWT auth for gitlab
 resource "vault_jwt_auth_backend" "jwt_gitlab" {
-  path  = "jwt"
-  oidc_discovery_url ="https://gitlab.0lzi.com"
-  bound_issuer = "https://gitlab.0lzi.com"
-  description = "JWT Auth for Gitlab"
+  path               = "jwt"
+  oidc_discovery_url = "https://gitlab.0lzi.com"
+  bound_issuer       = "https://gitlab.0lzi.com"
+  description        = "JWT Auth for Gitlab"
+}
+
+# Approle Auth
+resource "vault_auth_backend" "approle" {
+  type        = "approle"
+  description = "Machine Login with role_id and secret_id"
+}
+# BAO AGENT
+resource "vault_approle_auth_backend_role" "bao_agent" {
+  backend            = vault_auth_backend.approle.path
+  role_name          = "approle-auth"
+  token_policies     = ["default", "pki-cert", "approle-auth"]
+  secret_id_num_uses = 0
+}
+
+resource "vault_approle_auth_backend_role_secret_id" "id" {
+  backend   = vault_auth_backend.approle.path
+  role_name = vault_approle_auth_backend_role.bao_agent.role_name
+}
+
+# BOOTSTRAP
+
+resource "vault_approle_auth_backend_role" "bootstrap" {
+  backend        = vault_auth_backend.approle.path
+  role_name      = "agent-approle-auth"
+  token_policies = ["approle-restart"]
+}
+
+resource "vault_approle_auth_backend_role_secret_id" "bootstrap_id" {
+  backend               = vault_auth_backend.approle.path
+  role_name             = vault_approle_auth_backend_role.bootstrap.role_name
+  wrapping_ttl          = "300s"
+  with_wrapped_accessor = true
 }
--- openbao/policies.tf
+++ openbao/policies.tf
@@ -19,24 +19,61 @@ data "vault_policy_document" "readonly" {
   }
 }
 
+data "vault_policy_document" "approle_auth" {
+  rule {
+    path         = "auth/approle/role/*"
+    capabilities = ["create", "update", "read", "list"]
+    description  = "Create and Update approle"
+  }
+}
+
+data "vault_policy_document" "approle_restart" {
+  rule {
+    path         = "auth/approle/role/+/secret*"
+    capabilities = ["create", "update", "read"]
+    description  = "Create and Update approle"
+  }
+}
+
+data "vault_policy_document" "pki" {
+  rule {
+    path         = "pki_int/issue/0lzi-dot-internal-com"
+    capabilities = ["create", "update", ]
+    description  = "Create and update Certificates"
+  }
+}
 resource "vault_policy" "all" {
   name   = "all"
   policy = data.vault_policy_document.all.hcl
 }
 
 resource "vault_policy" "readonly" {
-   name = "read-only"
-   policy = data.vault_policy_document.readonly.hcl
+  name   = "read-only"
+  policy = data.vault_policy_document.readonly.hcl
 }
 
+resource "vault_policy" "approle-auth" {
+  name   = "approle-auth"
+  policy = data.vault_policy_document.approle_auth.hcl
+}
+
+resource "vault_policy" "approle-restart" {
+  name   = "approle-restart"
+  policy = data.vault_policy_document.approle_restart.hcl
+}
+
+resource "vault_policy" "pki-cert" {
+  name   = "pki-cert"
+  policy = data.vault_policy_document.pki.hcl
+}
 
 resource "vault_jwt_auth_backend_role" "gitlab-jwt" {
-  backend        = vault_jwt_auth_backend.jwt_gitlab.path
-  role_name      = "read-only"
-  token_policies = ["default", "read-only"]
+  backend         = vault_jwt_auth_backend.jwt_gitlab.path
+  role_name       = "read-only"
+  token_policies  = ["default", "read-only", "approle-restart"]
   bound_audiences = ["https://vault.0lzi.com"]
   user_claim      = "user_email"
-  token_ttl = "3600"
+  token_ttl       = "3600"
   role_type       = "jwt"
   bound_claims = {
     project_id = "2,6"
